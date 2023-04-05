81ae0f645668997f8d25dcb3dda316aa24cc5d9d
Holopotyliuk <golopotiluka@gmail.com>
2023-04-05 11:23:07 +0300

    create component list

--- src/components/listComponents/list/list.js
+++ src/components/listComponents/list/list.js
@@ -1,1 +1,10 @@
-
+import React from "react";
+import  './index.css'
+function List({ list }) {
+    return (
+        <li className="list">
+            <h2 onClick={() => console.log('clisk in the list')}>{list.title}</h2>
+        </li>
+    )
+}
+export default List
