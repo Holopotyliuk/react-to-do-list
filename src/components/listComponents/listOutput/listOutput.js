6cea387871cb03607446e4699a51ab71eda3e925
Holopotyliuk <golopotiluka@gmail.com>
2023-04-05 13:39:07 +0300

    add css in to listOutput and list

--- src/components/listComponents/listOutput/listOutput.js
+++ src/components/listComponents/listOutput/listOutput.js
@@ -1,8 +1,9 @@
 import React from "react";
 import List from "../list/list"
+import './index.css'
 function ListOutput({ lists }) {
     return (
-        <div>
+        <div className="list_form">
             {
                 lists.map(list => (
                     <List list={list} />
