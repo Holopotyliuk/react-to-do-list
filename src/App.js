4f0bf71f8f1a8a200028d1925a79b4bd13d642e5
Holopotyliuk <golopotiluka@gmail.com>
2023-04-05 11:20:41 +0300

    moved array lists in to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import List from './components/listComponents/list/list';
+
+const inc = (init = 0) => () => ++init;
+const getId = inc()
+let lists = [
+  {
+    listId: getId(),
+    title: 'First list'
+  },
+  {
+    listId: getId(),
+    title: 'Second list'
+  }];
+
 function App() {
   return (
-    <div className="App">
-      dfhgdfh
-    </div>
+    < div className="App" >
+      {
+        lists.map(list => (
+          <List list={list} />
+        ))
+      }
+    </div >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const inc = (init = 0) => () => ++init;
-const getId = inc()
-let lists = [
-  {
-    listId: getId(),
-    title: 'First list'
-  },
-  {
-    listId: getId(),
-    title: 'Second list'
-  }];
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
