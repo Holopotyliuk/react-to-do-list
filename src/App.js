984d2e06346c9a31fddabd8cd8d06356a458dbb3
Holopotyliuk <golopotiluka@gmail.com>
2023-04-05 13:55:10 +0300

    add array tasks

--- src/App.js
+++ src/App.js
@@ -5,17 +5,56 @@ const inc = (init = 0) => () => ++init;
 const getId = inc()
 let lists = [
   {
-    listId: getId(),
+    listId: 1,
     title: 'First list'
   },
   {
-    listId: getId(),
+    listId: 2,
     title: 'Second list'
   },
   {
-    listId: getId(),
+    listId: 3,
     title: 'Third list'
   }];
+
+let tasks = [
+  {
+    tasksid: getId(),
+    title: 'first task',
+    done: false,
+    due_date: new Date(),
+    listId: 1
+  },
+  {
+    tasksid: getId(),
+    title: 'Second task',
+    done: false,
+    due_date: new Date(),
+    listId: 1
+  },
+  {
+    tasksid: getId(),
+    title: 'first task from list two',
+    done: false,
+    due_date: new Date(),
+    listId: 2
+  },
+  {
+    tasksid: getId(),
+    title: 'second task from list two',
+    done: false,
+    due_date: new Date(),
+    listId: 2
+  },
+  {
+    tasksid: getId(),
+    title: 'first task from list three',
+    done: false,
+    due_date: new Date(),
+    listId: 3
+  },
+
+];
 
 function App() {
   return (
