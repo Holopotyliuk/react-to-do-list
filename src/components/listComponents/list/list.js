5f0ff070ef34349add0f005dda57cd4989cc67c5
Holopotyliuk <golopotiluka@gmail.com>
2023-04-08 16:03:15 +0300

    add button remove list

--- src/components/listComponents/list/list.js
+++ src/components/listComponents/list/list.js
@@ -2,8 +2,9 @@ import React from "react";
 import './index.css'
 function List({ list, setListId }) {
     return (
-        <li onClick={() => { (setListId(list.listid))}} className="list" >
-            <h2>{list.list_title}</h2>
+        <li className="list" >
+            <h2 onClick={() => { (setListId(list.listid)) }}>{list.list_title}</h2>
+            <button className="buttonRemove" onClick={() => { console.log('remove list') }}>X</button>
         </li>
     )
 }
